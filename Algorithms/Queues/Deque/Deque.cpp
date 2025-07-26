61a560a785ed8db16c23e28921d061f6e5812c52
Roubeq <arteem.blinov.2005@gmail.com>
2025-07-26 20:07:01 +0300

    deque completed

---
+++ Algorithms/Queues/Deque/Deque.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int data;
+	Node* next;
+};
+
+struct Deque {
+private:
+	int data;
+	Node* front, * rear;
+public:
+	
+	Deque() : front(nullptr), rear(nullptr) {}
+
+	bool isEmpty() {
+		if (front == nullptr && rear == nullptr) return 1;
+	}
+	void pushFront(int value) {
+		Node* temp = new Node;
+		temp->data = value;
+		if (isEmpty()) {
+			front = rear = temp;
+		} 
+		else {
+			temp->next = front;
+			front = temp;
+		}
+	}
+	void pushFront(int value) {
+		Node* temp = new Node;
+		temp->data = value;
+		if (isEmpty()) {
+			front = rear = temp;
+		}
+		else {
+			rear->next = temp;
+			rear = temp;
+		}
+	}
+}
+
+
+int main() {
+
+}
