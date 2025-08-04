1c084279b0846705403cd92143ba7eb60727e073
Roubeq <arteem.blinov.2005@gmail.com>
2025-08-04 23:34:47 +0300

    folders sorting and heaps added and projects(not all) completed

---
+++ Algorithms/Sorting/SelectionSort/SelectionSort.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void selectionSort(vector<int>& arr) {
+	int n = arr.size();
+	for (int i = 0; i < n - 1; i++) {
+		int minNumber = i;
+		for (int j = i + 1; j < n; j++) {
+			if (arr[j] < arr[minNumber]) {
+				minNumber = j;
+			}
+		}
+		int temp = arr[i];
+		arr[i] = arr[minNumber];
+		arr[minNumber] = temp;
+	}
+}
+
+void print(vector<int>& arr) {
+	for (int i : arr) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	vector<int> arr = {63,52,128,1,2,9,15,4,1240,24,3};
+	cout << "Исходный массив: " << endl;
+	print(arr);
+	
+	selectionSort(arr);
+
+	cout << "Массив после сортировки: " << endl;
+	print(arr);
+}
