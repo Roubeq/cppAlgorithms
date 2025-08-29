a61e392cbbbf29549409233392f88c74b9505d7a
Roubeq <arteem.blinov.2005@gmail.com>
2025-08-29 14:03:09 +0300

    factorial completed

---
+++ Algorithms/Recursion/Factorial/Factorial.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int factorial(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "Введите число, для которого хотите вычислить факториал: ";
+    cin >> n;
+    cout << factorial(n);
+}
+
+/*
+* Этапы формирования рекурсивной функции.
+* 1. Разбить задачу на более мелкие подзадачи.
+* 2. Написать базовый случай рекурсии(для выхода из неё).
+*/
