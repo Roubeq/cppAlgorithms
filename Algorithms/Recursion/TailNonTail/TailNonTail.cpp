c4a8437b7555a42e3773b89506a6bec1ef55d74f
Roubeq <arteem.blinov.2005@gmail.com>
2025-09-02 10:48:05 +0300

    tail and non-tail recursion completed

---
+++ Algorithms/Recursion/TailNonTail/TailNonTail.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+// Это хвостовая рекурсия - вызов её в конце, тем самым не нужно сохранять состояние
+void fun(int n) {
+	if (n == 0) {
+		return;
+	}
+	else {
+		cout << n << " ";
+	}
+	return fun(n - 1);
+}
+
+// Это не хвостовая рекурсия, ей как раз таки необходимо проверять что за ней идёт
+void funNonTail(int n) {
+	if (n == 0) {
+		return;
+	}
+	funNonTail(n - 1);
+	cout << n << " ";
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	fun(3);
+	cout << endl;
+	funNonTail(3);
+	return 0;
+}
