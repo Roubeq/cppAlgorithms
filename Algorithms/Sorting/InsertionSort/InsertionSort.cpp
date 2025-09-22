a4d1a6805e8ba35e9919d3bdfab6ba0cd9c16f9a
Roubeq <arteem.blinov.2005@gmail.com>
2025-09-22 20:46:38 +0300

    insertion sort with LL using completed

--- Algorithms/Sorting/InsertionSort/InsertionSort.cpp
+++ Algorithms/Sorting/InsertionSort/InsertionSort.cpp
@@ -3,6 +3,50 @@
 
 using namespace std;
 
+
+struct Node {
+	int val;
+	Node* next;
+	Node() : val(0),next(nullptr) {};
+	Node(int value) : val(value),next(nullptr){}
+};
+
+
+void addNode(Node*& head, int value) {
+	Node* temp = new Node(value);
+	temp->next = head;
+	head = temp;
+}
+
+
+Node* insertionSortLL(Node* head) {
+	if (head == nullptr) return head;
+
+	Node* dummy = new Node(0);
+	Node* prev = dummy;
+
+	while (head != nullptr) {
+		Node* next = head->next;
+
+		if (prev->val >= head->val) {
+			prev = dummy;
+		}
+
+		while (prev->next && prev->next->val < head->val) {
+			prev = prev->next;
+		}
+		head->next = prev->next;
+		prev->next = head;
+
+		head = next;
+	}
+
+	return dummy->next;
+
+}
+
+
+// С помощью вектора
 void insertionSort(vector<int>& arr) {
 	int n = arr.size();
 	for (int i = 1; i < n; i++) {
@@ -23,9 +67,24 @@ void print(vector<int>& arr) {
 	cout << endl;
 }
 
+void printLL(Node* head) {
+	Node* temp = head;
+	while (temp != nullptr) {
+		cout << temp->val << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
 int main() {
 	setlocale(LC_ALL, "Russian");
 	vector<int> arr = { 5235,12,4123,1,2,67,17,195,13 };
+	Node* head = nullptr;
+	addNode(head, 1);
+	addNode(head, 5);
+	addNode(head, 3);
+	addNode(head, 2);
+	addNode(head, 4);
 	
 	cout << "Исходный массив: " << endl;
 	cout << arr.size() << endl;
@@ -35,4 +94,13 @@ int main() {
 
 	cout << "Итоговый массив: " << endl;
 	print(arr);
+
+
+	cout << "Исходный связный список: " << endl;
+	printLL(head);
+
+	head = insertionSortLL(head);
+
+	cout << "Итоговый связный список: " << endl;
+	printLL(head);
 }
